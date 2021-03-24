0dfd1ad480345c56616dc3ec17f52aab782b9818
Yadira11 <chavarriayadira85@gmail.com>
2021-03-24 13:04:32 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var quien = [
+    "my father",
+    "my mom",
+    "my cat",
+    "my dog",
+    "my sister",
+    "my brother"
+  ];
+  var que = [
+    "I force myself to stay",
+    "I force myself to take him",
+    "He attacked me leaving",
+    "ate",
+    "I forget ",
+    "Got sick on"
+  ];
+  var donde = [
+    "at home",
+    "to the supermarket",
+    "my room",
+    "my homework",
+    "my car keys",
+    "the trip"
+  ];
+
+  var sentencia =
+    quien[aleatorio(quien.length)] +
+    " " +
+    que[aleatorio(que.length)] +
+    " " +
+    donde[aleatorio(donde.length)];
+
+  let excusaSpan = document.querySelector("#excusa");
+  excusaSpan.innerHTML = sentencia;
 };
+
+function aleatorio(length) {
+  let numero = Math.floor(Math.random() * length);
+  return numero;
+}
